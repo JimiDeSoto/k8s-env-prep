e04aad24849d9d5346448dbc5c4cbd188bff3a5f
JimiDeSoto <51928786+JimiDeSoto@users.noreply.github.com>
2019-11-10 15:28:55 +0100

    Create pk8s.sh
    clone https://github.com/PoznajKubernetes/bash-theme/blob/master/poznajkubernetes.sh

---
+++ pk8s.sh
@@ -0,0 +1,24 @@
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.pk8s-bash/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+source <(kubectl completion bash)
+alias k=kubectl
+complete -F __start_kubectl k
+
+#kubectx and kubens
+export PATH=~/.kubectx:$PATH
+
+KUBE_PS1_SYMBOL_DEFAULT=${KUBE_PS1_SYMBOL_DEFAULT:-$'\u2388'} # k8s icon without space
+KUBE_PS1_NS_COLOR="142"        #from YELLOW 09
+KUBE_PS1_CTX_COLOR="37"        #from TEAL 09
+
+source  ~/.kube-ps1/kube-ps1.sh
+PS1='[\[\033[38;5;${KUBE_PS1_NS_COLOR}m\]\u\[\033[m\]@\[\033[38;5;${KUBE_PS1_CTX_COLOR}m\]\h:\[\033[33;m\]\W\[\033[m\] $(kube_ps1)]\n\$ '
